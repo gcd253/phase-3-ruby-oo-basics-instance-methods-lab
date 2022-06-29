06057f3191ab6a7d29bdc1d6ce70b1f4ccd69af0
Griffin Dooley <dooleygriffin@gmail.com>
2022-06-29 10:49:34 -0400

    commit tree d8129cc50bbd4e662e8b42bddaee0d1b078718f8

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+  end
+
+  journey = Dog.new
+  journey.bark
